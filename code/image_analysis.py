76684c5e304e1fb8c6406db5d0db8ca73f66a583
setsanto <nicholas.miles6@gmail.com>
2018-03-07 21:39:52 -0500

    brian and nick UPlounge edits

--- code/image_analysis.py
+++ code/image_analysis.py
@@ -1,21 +1,22 @@
 import os
 import operator
 import random
-from sklearn.utils import shuffle
+from sklearn import metrics
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
 import cv2
+from imutils import resize
 
 
-def read_image_loc(filepath):
-    images = []
+def random_image(filepath):
+    file = random.choice(os.listdir(filepath))
 
-    for file in os.listdir(filepath):
-        images.append(cv2.cvtColor(cv2.imread(filepath + file), cv2.COLOR_BGR2RGB))
+    raw_image = cv2.cvtColor(cv2.imread(filepath + file), cv2.COLOR_BGR2RGB)
+    resized_image = resize(raw_image, width=50)
 
-    return images
+    return resized_image
 
 
 def main_colour(image_file, ignored_param=5, top_colours=1):
@@ -66,7 +67,7 @@ def plot_colors(hist, centroids):
     bar = np.zeros((50,300,3), dtype = 'uint8')
     startX = 0
 
-    for (percent, color) in zip(hist, centroids):
+    for (percent, color) in sorted(zip(hist, centroids), key=lambda x: -x[0]):
         endX = startX + (percent * 300)
         cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype('uint8').tolist(), -1)
         startX = endX
@@ -74,32 +75,59 @@ def plot_colors(hist, centroids):
     return bar
 
 
-def cluster_image(image_file, clusters=20):
+def cluster_image(image_file, clusters=5):
     image_arr = image_file.reshape((image_file.shape[0] * image_file.shape[1], 3))
 
     clt = KMeans(n_clusters = clusters)
     clt.fit(image_arr)
 
-    return clt
+    silhouettes = metrics.silhouette_score(image_arr, clt.labels_, metric='euclidean', sample_size=300)
 
+    return clt, silhouettes
 
-IMAGE_LIST = read_image_loc('../data/posters/')
-SELECTED_IMAGE = random.choice(IMAGE_LIST)
+if __name__ == '__main__':
+    SELECTED_IMAGE = random_image('../data/posters/')
 
-plt.figure()
-plt.imshow(SELECTED_IMAGE)
-plt.axis('off')
+    max_score = 0
+    optimal_k = 0
+    optimal_clt = None
 
-IMAGE_CLT = cluster_image(SELECTED_IMAGE, 5)
-COLOUR_HISTOGRAM = centroid_histogram(IMAGE_CLT)
-COLOUR_BAR = plot_colors(COLOUR_HISTOGRAM, IMAGE_CLT.cluster_centers_)
+    colour_bars = []
+    for cluster_size in range(2,17):
+        CLT, SILHOUETTE_SCORE = cluster_image(SELECTED_IMAGE, cluster_size)
+        if SILHOUETTE_SCORE > max_score:
+            max_score = SILHOUETTE_SCORE
+            optimal_k = cluster_size
+            optimal_clt = CLT
 
-plt.figure()
-plt.axis('off')
-plt.imshow(COLOUR_BAR)
-plt.show()
+        COLOUR_HISTOGRAM = centroid_histogram(CLT)    
+        COLOUR_BAR = plot_colors(COLOUR_HISTOGRAM,
+                                 CLT.cluster_centers_)
+        colour_bars.append(COLOUR_BAR)
 
-# TOP_COLOUR_LIST = main_colour(SELECTED_IMAGE, top_colours=5)
+    IMAGE_CLT = optimal_clt
+    print(optimal_k)
+    print(max_score)
+
+    cbars = np.vstack(colour_bars)
+
+    COLOUR_HISTOGRAM = centroid_histogram(IMAGE_CLT)
+    COLOUR_BAR = plot_colors(COLOUR_HISTOGRAM, IMAGE_CLT.cluster_centers_)
+
+    plt.figure()
+    plt.imshow(SELECTED_IMAGE)
+    plt.axis('off')
+
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(COLOUR_BAR)
+
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(cbars)
+    plt.show()
+
+    # TOP_COLOUR_LIST = main_colour(SELECTED_IMAGE, top_colours=5)
 
 # for i in TOP_COLOUR_LIST:
 #     for j in TOP_COLOUR_LIST:
